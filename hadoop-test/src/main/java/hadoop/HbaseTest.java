2ede4bd58eda31ff3072140b339d8918007a95f4
wutao123456 <wutao56789@gmail.com>
2019-05-14 14:59:11 +0800

    hbase api demo

--- hadoop-test/src/main/java/hadoop/HbaseTest.java
+++ hadoop-test/src/main/java/hadoop/HbaseTest.java
@@ -3,9 +3,11 @@ package hadoop;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -99,11 +101,90 @@ public class HbaseTest {
         List<Get> gets = new ArrayList<Get>();
         for(int i=0;i<10;i++){
             Get get = new Get(Bytes.toBytes("rowkey_"+i));
+            get.addFamily(Bytes.toBytes("i"));
+            get.addFamily(Bytes.toBytes("j"));
             gets.add(get);
         }
-        Result[] results = table.get(gets);
-        for(Result result:results){
-            //使用cell获取result里面的数据
+
+        Get get = new Get(Bytes.toBytes("rowkey_0"));
+        get.addFamily(Bytes.toBytes("j"));
+        get.addColumn(Bytes.toBytes("j"),Bytes.toBytes("phone"));
+        Result result = table.get(get);
+        CellScanner cellScanner = result.cellScanner();
+        while(cellScanner.advance()){
+            Cell cell = cellScanner.current();
+            //从单元格cell中把数据获取并输出
+            //使用 CellUtil工具类，从cell中把数据获取出来
+            String famliy = Bytes.toString(CellUtil.cloneFamily(cell));
+            String qualify = Bytes.toString(CellUtil.cloneQualifier(cell));
+            String rowkey = Bytes.toString(CellUtil.cloneRow(cell));
+            String value = Bytes.toString(CellUtil.cloneValue(cell));
+            System.out.println("rowkey:"+rowkey+",columnfamily:"+famliy+",qualify:"+qualify+",value:"+value);
+        }
+
+
+
+//        Result[] results = table.get(gets);
+//        for(Result result:results){
+//            //使用cell获取result里面的数据
+//            CellScanner cellScanner = result.cellScanner();
+//            while(cellScanner.advance()){
+//                Cell cell = cellScanner.current();
+//                //从单元格cell中把数据获取并输出
+//                //使用 CellUtil工具类，从cell中把数据获取出来
+//                String famliy = Bytes.toString(CellUtil.cloneFamily(cell));
+//                String qualify = Bytes.toString(CellUtil.cloneQualifier(cell));
+//                String rowkey = Bytes.toString(CellUtil.cloneRow(cell));
+//                String value = Bytes.toString(CellUtil.cloneValue(cell));
+//                System.out.println("rowkey:"+rowkey+",columnfamily:"+famliy+",qualify:"+qualify+",value:"+value);
+//            }
+//        }
+    }
+
+    public void scanFilter() throws Exception{
+        TableName tableName = TableName.valueOf("customer");
+        table = connection.getTable(tableName);
+        Scan scan = new Scan();
+        SingleColumnValueFilter filter = new SingleColumnValueFilter(
+                Bytes.toBytes("i"),
+                Bytes.toBytes("birthday"),
+                CompareOperator.EQUAL,
+                Bytes.toBytes("20170001")
+        );
+
+        //列族过滤器
+        FamilyFilter familyFilter = new FamilyFilter(CompareOperator.EQUAL,new BinaryComparator("j".getBytes()));
+        //列名过滤器
+        QualifierFilter qualifierFilter = new QualifierFilter(CompareOperator.EQUAL,new BinaryComparator("email".getBytes()));
+        //列前缀过滤器
+        ColumnPrefixFilter columnPrefixFilter = new ColumnPrefixFilter("a".getBytes());
+        byte [][] prefixs = new byte[][]{Bytes.toBytes("a"),Bytes.toBytes("u")};
+        //多个列前缀过滤器
+        MultipleColumnPrefixFilter multipleColumnPrefixFilter = new MultipleColumnPrefixFilter(prefixs);
+        //行键过滤器
+        RowFilter rowFilter = new RowFilter(CompareOperator.EQUAL,new BinaryComparator("rowkey_0".getBytes()));
+        //第一个键过滤器
+        FirstKeyOnlyFilter firstKeyOnlyFilter = new FirstKeyOnlyFilter();
+        //键过滤器
+        KeyOnlyFilter keyOnlyFilter = new KeyOnlyFilter();
+        //列数过滤器
+        ColumnCountGetFilter columnCountGetFilter = new ColumnCountGetFilter(3);
+        //列值过滤器
+        ValueFilter valueFilter = new ValueFilter(CompareOperator.EQUAL,new BinaryComparator("email_8".getBytes()));
+        SkipFilter skipFilter = new SkipFilter(valueFilter);
+        //分页过滤器
+        PageFilter pageFilter = new PageFilter(3);
+        //过滤器组合(AND 或 OR)
+        FilterList filterList = new FilterList(FilterList.Operator.MUST_PASS_ALL);
+        filterList.addFilter(rowFilter);
+        filterList.addFilter(qualifierFilter);
+        scan.setFilter(pageFilter);
+//        scan.setFilter(filterList);
+        ResultScanner scanner = table.getScanner(scan);
+
+        Iterator<Result> iterator = scanner.iterator();
+        while (iterator.hasNext()){
+            Result result = iterator.next();
             CellScanner cellScanner = result.cellScanner();
             while(cellScanner.advance()){
                 Cell cell = cellScanner.current();
@@ -124,8 +205,9 @@ public class HbaseTest {
 
     public static void main(String[] args)throws Exception{
         HbaseTest hbaseTest = new HbaseTest();
-        hbaseTest.createTable("test", "c1","c2");
+//        hbaseTest.createTable("test", "c1","c2");
 //        hbaseTest.putData();
 //        hbaseTest.getData();
+        hbaseTest.scanFilter();
     }
 }
