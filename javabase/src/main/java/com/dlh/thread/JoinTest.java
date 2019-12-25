84c39b51134acbdf18aeabbeb4abd6f54778ea1f
wutao <wutao@duoliheng.com>
2019-12-25 09:43:55 +0800

    add comments

--- javabase/src/main/java/com/dlh/thread/JoinTest.java
+++ javabase/src/main/java/com/dlh/thread/JoinTest.java
@@ -2,7 +2,7 @@ package com.dlh.thread;
 
 public class JoinTest {
 
-    static class Jin extends Thread{
+    static class Jin implements Runnable{
         @Override
         public void run() {
             for (int i = 0; i <1000 ; i++) {
@@ -12,10 +12,18 @@ public class JoinTest {
     }
 
     public static void main(String[] args) throws InterruptedException {
+
         Jin t1 = new Jin();
         Jin t2 = new Jin();
-        t1.start();
-//        t1.join();
-        t2.start();
+        Thread thread1 = new Thread(t1,"t1");
+        Thread thread2 = new Thread(t2,"t2");
+        thread1.start();
+
+        thread2.start();
+        thread1.join();
+        //在main线程里调用thread1.join(),main会阻塞到thread1执行完毕再执行下面
+
+
+        System.out.println("thread main");
     }
 }
