15f2170091d96d161adef93527c3675ae99ed9c2
wutao <wutao@duoliheng.com>
2020-01-15 23:15:32 +0800

    java-agent

--- javaagent/src/main/java/com/dlh/App.java
+++ javaagent/src/main/java/com/dlh/App.java
@@ -1,6 +1,9 @@
 package com.dlh;
 
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
 
 /**
  * Hello world!
@@ -15,6 +18,7 @@ public class App {
     public static void premain(String agentArgs, Instrumentation inst){
         System.out.println("=========premain方法执行1========");
         System.out.println(agentArgs);
+        inst.addTransformer(new DefineTransformer(),true);
     }
 
     /**
@@ -25,5 +29,13 @@ public class App {
     public static void premain(String agentArgs){
         System.out.println("=========premain方法执行2========");
         System.out.println(agentArgs);
+    }
+
+    static class DefineTransformer implements ClassFileTransformer{
+        @Override
+        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+            System.out.println("premain load class " + className);
+            return classfileBuffer;
+        }
     }
 }
