afc139d4de4d012a1ae42aabd923e8f7234ff8d3
wutao <wutao56789@gmail.com>
2020-02-18 11:54:14 +0800

    javaagent test

---
+++ javaagent/src/main/java/com/dlh/AgentMainTest.java
@@ -0,0 +1,27 @@
+package com.dlh;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+/**
+ * @author wutao
+ * @email wutao56789@gmail.com
+ * @date 2020/2/17 21:45
+ */
+public class AgentMainTest {
+
+    public static void agentmain(String agentArgs, Instrumentation instrumentation) {
+        instrumentation.addTransformer(new DefineTransformer(), true);
+    }
+
+    static class DefineTransformer implements ClassFileTransformer {
+
+        @Override
+        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+            System.out.println("agent load Class:" + className);
+            return classfileBuffer;
+        }
+    }
+}
